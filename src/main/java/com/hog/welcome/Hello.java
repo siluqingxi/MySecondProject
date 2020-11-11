7aff5f7dc78828db54c499adcb806c4af96ff432
halfofgame <halfofgame@foxmail.com>
2020-11-11 20:21:09 +0800

    第二次提交。

--- src/main/java/com/hog/welcome/Hello.java
+++ src/main/java/com/hog/welcome/Hello.java
@@ -2,6 +2,7 @@ package com.hog.welcome;
 
 public class Hello {
     public static void main(String[] args) {
+        System.out.println("First line");
         System.out.println("Hello world");
     }
 }
